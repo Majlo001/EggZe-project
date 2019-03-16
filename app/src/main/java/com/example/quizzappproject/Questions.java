322b2e2fb5dbcdb5e4e9c59aecf57516f4f461b8
Majlo001 <45098860+Majlo001@users.noreply.github.com>
2019-03-16 12:00:27 +0100

    1

---
+++ app/src/main/java/com/example/quizzappproject/Questions.java
@@ -0,0 +1,34 @@
+package com.example.quizzappproject;
+
+/*===== Made by Majlo on 11.03.2019 =====*/
+
+public class Questions {
+    public String mQuestions [] = {
+            "Ile planet ma Układ Słoneczny?"
+            "Jaka jest druga najbliższa Ziemi gwiazda?"
+            "Jak nazywa się pierwszy obiekt, który został umieszczony na orbicie Ziemi?"
+            "Kto jako pierwszy wysłał człowieka w kosmos?"
+            "Jak nazywała się misja pierwszego kosmicznego lotu załogowego?"
+            "Teoria inflacji zakłada, że wszechświat powstał..?"
+            "Eksperyment BOOMERanG przeprowadzony w latach 1998 i 2003 udowodnił, że Wszechświat jest..?"
+            "Która gwiazda (nie uwzględniając Słońca) jest najjaśniejsza według jasności widomej?"
+            "Einstein urodził się w dzień liczby Pi (14 Marca), a który wielki uczony zmarł w tym dniu?"
+
+    };
+
+    private String mChoices [][] = {
+            {"6","7","8","9"},
+            {"Słońce","Alfa Centauri A","Proxima Centauri","Syriusz"},
+            {"Łajka","Sputnik 1","Sputnik 2","Apollo 1"},
+            {"SpaceX","NASA","ZSRR","ESA"},
+            {"Wodstok 1","Wostok 1","Apollo 11","Gemini 1"},
+            {"z fałszywej próżni","z ciemnej materii","z rozpadu protonów","z próżni doskonałej"},
+            {"płaski","ma kształt elipsy","holograficzną iluzją","ma cztery wymiary"},
+            {"Alfa Centauri B","Alfa Centauri A","Proxima Centauri","Syriusz"},
+            {"Jan Heweliusz","Issac Newton","Blaise Pascal","Stephen Hawking"},
+
+    };
+
+    public String mCorrectAnswer [] = {"8","Proxima Centauri","Sputnik 1","ZSRR","Wostok 1","z fałszywej próżni","płaski","Syriusz","Stephen Hawking"}
+
+}
